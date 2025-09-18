a4766005bccc7cc65a270c76649af322a5a134c2
Rohit Savalagi <rohitsavalagi70@gmail.com>
2025-09-18 08:35:24 +0530

    basic in-memry-crud application

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import 'dotenv/config';
-import express, { Request, Response } from 'express';
-import mongoose from 'mongoose';
-import todoRouter from './src/routes/todos.route';
-import { errorHandler } from './src/middlewares/error-handler';
-import morgan from 'morgan';
-import logger from './src/utils/error.logger';
-import coros from 'cors';
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import todoRouter from "./src/routes/todos.route";
+import { errorHandler } from "./src/middlewares/error-handler";
+import morgan from "morgan";
+import logger from "./src/utils/error.logger";
+import coros from "cors";
 
 const app = express();
 
@@ -18,21 +18,22 @@ app.use(
     },
   })
 );
-app.use('/todos', todoRouter);
+app.use("/todos", todoRouter);
 app.use(errorHandler);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello world from Typescript');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world from Typescript");
 });
 
-mongoose.connect(process.env.MONGODB_URI ?? '')
-    .then(() => {
-        console.log('✅ Connected to local MongoDB');
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running at http://localhost:${process.env.PORT}`);
-        });
-    })
-    .catch(error => {
-        console.error('❌ MongoDB connection error:', error);
-        process.exit(1); // Exit process with failure
+mongoose
+  .connect(process.env.MONGODB_URI ?? "")
+  .then(() => {
+    console.log("✅ Connected to local MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running at http://localhost:${process.env.PORT}`);
     });
+  })
+  .catch((error) => {
+    console.error("❌ MongoDB connection error:", error);
+    process.exit(1);
+  });
